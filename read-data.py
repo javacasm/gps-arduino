5a16d6bb72f63cb449c3bdc2c379c0c771c5344e
javacasm <javacasm@gmail.com>
2015-10-02 17:32:05 +0200

    sin doc

--- read-data.py
+++ read-data.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+# NMEA  sintax http://aprs.gids.nl/nmea/
+
+$GPRMC - Recommended minimum specific GPS/Transit data
+$GPRMC,152316.00,V,,,,,,,021015,,,N*78
+$GPRMC,hhmmss.ss,A,llll.ll,a,yyyyy.yy,a,x.x,x.x,ddmmyy,x.x,
+
 from time import sleep
 import serial
 try:
@@ -9,7 +15,7 @@ try:
 	while True:
 		try:
 		   	serial_data = ser.read()
-		   	if serial_data == 10:
+		   	if serial_data == '\n':
 		   		print linea
 		   		linea=''
 		   	else:		
