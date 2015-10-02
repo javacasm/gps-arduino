99fdf123d668918529f1a0f4dfc8a65ece6e8349
javacasm <javacasm@gmail.com>
2015-10-02 22:13:06 +0200

    sin doc

--- read-data.py
+++ read-data.py
@@ -27,3 +27,36 @@ except:
 finally:
     if ser:
         ser.close()
+
+# Calculate checksums 
+# http://doschman.blogspot.com.es/2013/01/calculating-nmea-sentence-checksums.html
+
+def chksum_nmea(sentence):
+    
+    # This is a string, will need to convert it to hex for 
+    # proper comparsion below
+    cksum = sentence[len(sentence) - 2:]
+    
+    # String slicing: Grabs all the characters 
+    # between '$' and '*' and nukes any lingering
+    # newline or CRLF
+    chksumdata = re.sub("(\n|\r\n)","", sentence[sentence.find("$")+1:sentence.find("*")])
+    
+    # Initializing our first XOR value
+    csum = 0 
+    
+    # For each char in chksumdata, XOR against the previous 
+    # XOR'd char.  The final XOR of the last char will be our 
+    # checksum to verify against the checksum we sliced off 
+    # the NMEA sentence
+    
+    for c in chksumdata:
+       # XOR'ing value of csum against the next char in line
+       # and storing the new XOR value in csum
+       csum ^= ord(c)
+    
+    # Do we have a validated sentence?
+    if hex(csum) == hex(int(cksum, 16)):
+       return True
+
+    return False
