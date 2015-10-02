ea6730168214d648828825ee229606413b772f66
javacasm <javacasm@gmail.com>
2015-10-02 17:19:15 +0200

    python

--- read-data.py
+++ read-data.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 from time import sleep
 import serial
-ser = serial.Serial('/dev/ttyUSB1', 9600) # Establish the connection on a specific port
-#ser.open()
+try:
+	ser = serial.Serial('/dev/ttyUSB1', 9600) # Establish the connection on a specific port
+	#ser.open()
 
-while True:
-	try:
-	   	serial_data = ser.read()
-		print serial_data
-	except serial.serialutil.SerialException:
-		#print "error"
-		pass
+	while True:
+		try:
+		   	serial_data = ser.read()
+			print serial_data
+		except serial.serialutil.SerialException:
+			#print "error"
+			pass
+except:
+    pass    
+finally:
+    if ser:
+        ser.close()
