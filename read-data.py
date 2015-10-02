bbe9785936e3c505cfed938a4077e671954bf8b3
javacasm <javacasm@gmail.com>
2015-10-02 17:42:39 +0200

    python

--- read-data.py
+++ read-data.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # NMEA  sintax http://aprs.gids.nl/nmea/
 
-$GPRMC - Recommended minimum specific GPS/Transit data
-$GPRMC,152316.00,V,,,,,,,021015,,,N*78
-$GPRMC,hhmmss.ss,A,llll.ll,a,yyyyy.yy,a,x.x,x.x,ddmmyy,x.x,
-
 from time import sleep
 import serial
 try:
